69dbbe5adc000ddd75a15b5833e75abad7a75642
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-19 20:08:58 +0100

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from '../models/user.model';
+import User from '../models/user.js';
 
 // Controller function for user registration
 export const register = async (req, res) => {
