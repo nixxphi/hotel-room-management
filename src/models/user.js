57e44debda97f9e06c9e4a007a607f66d4c9b3af
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-19 09:12:41 +0100

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,23 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['guest', 'admin'], default: 'guest' }
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['guest', 'admin'],
+    default: 'guest'
+  },
+  
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+export default User;
