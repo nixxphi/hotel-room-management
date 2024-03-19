776e96333e8329c91de322045508a064d86cd49f
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-19 09:10:58 +0100

    Update roomTypeRouter.js

--- src/routes/roomTypeRouter.js
+++ src/routes/roomTypeRouter.js
@@ -2,8 +2,29 @@ import express from 'express';
 import { Router } from 'express';
 import * as roomTypeController from '../controllers/roomTypeController';
 import { asyncHandler } from '../../../utils';
+import authMiddleware from '../middlewares/authMiddleware';
+import authorizationMiddleware from '../middlewares/authorizationMiddleware';
+import validationMiddleware from '../middlewares/validationMiddleware';
+import Joi from 'joi';
 
-// Create a new router instance
+// Adding validation schema for creating a new room type
+const roomTypeSchema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string(),
+  price: Joi.number(),
+  amenities: Joi.string(),
+  image: Joi.string(),
+  isAvailable: Joi.boolean()
+});
+
+// POST endpoint for creating a new room type
+router.post('/api/v1/room-types', 
+  authMiddleware,
+  authorizationMiddleware,
+  validationMiddleware(roomTypeSchema),
+  roomTypeController.createRoomType
+);
+
 const router = Router();
 
 // POST endpoint for creating room types
