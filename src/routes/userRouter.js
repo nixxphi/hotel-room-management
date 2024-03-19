2a59c90011553a7478dd4889238deb1a790a234e
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-19 09:53:36 +0100

    Create userRouter.js

---
+++ src/routes/userRouter.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { register, login } from '../controllers/userController';
+import { asyncHandler } from '../../../utils';
+
+const router = express.Router();
+
+// POST endpoint for user registration
+router.post('/register', asyncHandler(register));
+
+// POST endpoint for user login
+router.post('/login', asyncHandler(login));
+
+export default router;
