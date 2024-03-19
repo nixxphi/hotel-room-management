4412d67b30dc95c9ec95d8575c060e1ce4bf5126
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-19 08:28:30 +0100

    Update authorizationMiddleware.js

--- src/middlewares/authorizationMiddleware.js
+++ src/middlewares/authorizationMiddleware.js
@@ -1,6 +1,6 @@
 const authorizationMiddleware = (req, res, next) => {
   if (req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Access denied. You are not authorized to perform this action.' });
+    return res.status(403).json({ message: 'Access denied. You are not authorized to perform this action. Come back when you\'re an admin.' });
   }
   next();
 };
