b58b77ee8bd14d1661ccebe11366d8aba08b258c
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-19 08:09:18 +0100

    Create authorizationMiddleware.js

---
+++ SRC/middlewares/authorizationMiddleware.js
@@ -0,0 +1,8 @@
+const authorizationMiddleware = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Access denied. You are not authorized to perform this action.' });
+  }
+  next();
+};
+
+module.exports = authorizationMiddleware;
