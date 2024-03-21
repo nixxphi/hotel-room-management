7d4a3f8a223c81ca8b1b9e9d7423ac92335716a8
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-21 17:53:32 +0100

    Update RoomType.js

--- src/models/RoomType.js
+++ src/models/RoomType.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 
-// DEFINING ROOMS
+// Define a new schema for rooms
 const roomTypeSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    unique: true,
+    required: true
+  },
   name: {
     type: String,
     required: true
@@ -36,7 +41,16 @@ const roomTypeSchema = new mongoose.Schema({
   }
 });
 
-// CREATING THE ROOMTYPE MODEL USING THE ROOMTYPESCHEMA
+// Adding a pre-save middleware to generate the ID before saving the document
+roomTypeSchema.pre('save', async function (next) {
+  // To generate the sequential ID
+  const count = await RoomType.countDocuments();
+  const paddedCount = String(count + 1).padStart(5, '0'); // Padding with zeros to ensure 5 digits cuz... HTR-1 doesn't look as good as HTR-00001.
+  this.id = `HTR-${paddedCount}`;
+  next();
+});
+
+// Creating the RoomType model
 const RoomType = mongoose.model('RoomType', roomTypeSchema);
 
 export default RoomType;
