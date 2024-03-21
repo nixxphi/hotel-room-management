a2da12153c5ea43ef45ed7f48dfc7f5d11aaada3
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-22 00:30:14 +0100

    Update validationMiddleware.js

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -1,11 +1,21 @@
-const Joi = require('joi');
 
-const validationMiddleware = (schema) => {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) return res.status(400).json({ message: error.details[0].message });
-    next();
-  };
+import Joi from 'joi';
+
+const validationMiddleware = (req, res, next) => {
+  const roomTypeSchema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string(),
+    price: Joi.number(),
+    amenities: Joi.string(),
+    image: Joi.string(),
+    isAvailable: Joi.boolean()
+  });
+
+  const { error } = roomTypeSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
 };
 
-module.exports = validationMiddleware;
+export default validationMiddleware;
