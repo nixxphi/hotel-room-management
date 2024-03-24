c46378889b2329b6007e696eef3c1d52e47a1e24
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-24 07:57:35 +0100

    Update and rename user.js to user.model.js

---
+++ src/models/user.model.js
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose' 
+  
+ const userSchema = new Schema({ 
+     filename: { 
+         type: String, 
+         required: true 
+     },  
+     secure_url: { 
+         type: String, 
+         required: true 
+     }, 
+     deleted: { 
+         type: Boolean, 
+         default: false 
+     } 
+ }, {timestamps: true}) 
+  
+ export default model('User', userSchema)
