7a556f87367382e013bda281e134012f31c172e8
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-24 08:41:17 +0100

    Create room.validate.js

---
+++ src/validation/room.validate.js
@@ -0,0 +1,18 @@
+
+import Joi from 'joi';
+import validate from '../middleware/validate.middleware.js';
+
+const roomsValidationSchema = {
+  body: Joi.object({
+name: Joi.string().required(),
+    description: Joi.string(),
+    price: Joi.number(),
+    amenities: Joi.string(),
+    image: Joi.string(),
+    isAvailable: Joi.boolean()
+  })
+};
+
+const roomsValidationMiddleware = validate(roomsValidationSchema);
+
+export default roomsValidationMiddleware;
