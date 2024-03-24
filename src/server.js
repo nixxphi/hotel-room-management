1a9aac79d4a49101eace1a1de492217b98bf58c4
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-24 11:07:07 -0700

    all that

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
  import app from "./app.js"; 
  import { logger } from "./utils/logger.js"; 
   
- const PORT = process.env.PORT; 
+ const PORT = process.env.PORT|| 5000; 
   
  app.listen(PORT, () => { 
    logger.info(`listening on port ${PORT}`); 
